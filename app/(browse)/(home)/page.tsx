b808a2d421b2c39b2d9964e23473813312152f81
awais512 <raza.awais25@gmail.com>
2024-03-10 22:58:36 +0000

    Navbar Actions

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -1,12 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
-
 export default function Home() {
   return (
-    <>
-      <div>
-        Dashboard
-        <UserButton afterSignOutUrl="/" />
-      </div>
-    </>
+    <div className="flex flex-col gap-y-4">
+      <h1>Homepage</h1>
+    </div>
   );
 }
