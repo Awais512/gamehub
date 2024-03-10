7fa23dc9c7caef943af39b029430e93cf0fbdb95
awais512 <raza.awais25@gmail.com>
2024-03-10 22:36:40 +0000

    Navbar

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -3,8 +3,10 @@ import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <>
-      <p>Only authenticated users can see this</p>
-      <UserButton afterSignOutUrl="/" />
+      <div>
+        Dashboard
+        <UserButton afterSignOutUrl="/" />
+      </div>
     </>
   );
 }
---
+++ app/(browse)/_components/logo.tsx
@@ -0,0 +1,25 @@
+import { cn } from "@/lib/utils";
+import { Poppins } from "next/font/google";
+import Image from "next/image";
+import Link from "next/link";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
+});
+
+export const Logo = () => {
+  return (
+    <Link href="/">
+      <div className="hidden lg:flex items-center gap-x-4 hover:opacity-75 transition">
+        <div className="bg-white rounded-full p-1">
+          <Image src="/spooky.svg" alt="Logo" height="32" width="32" />
+        </div>
+        <div className={cn(font.className)}>
+          <p className="text-lg font-semibold">Gamehub</p>
+          <p className="text-xs text-muted-foreground">Let&apos;s Play</p>
+        </div>
+      </div>
+    </Link>
+  );
+};
