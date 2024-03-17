7aa29f5c4b0d8b557b0e0e7242bb2f6e8b551212
awais512 <raza.awais25@gmail.com>
2024-03-17 22:33:38 +0000

    Keys Page

---
+++ app/(dashboard)/u/[username]/keys/page.tsx
@@ -0,0 +1,29 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+import { UrlCard } from "./_ components/url-card";
+import { getSelf } from "@/lib/auth-service";
+import { getStreamByUserId } from "@/lib/stream-service";
+import { KeyCard } from "./_ components/key-card";
+
+const KeysPage = async () => {
+  const self = await getSelf();
+  const stream = await getStreamByUserId(self.id);
+
+  if (!stream) {
+    throw new Error("Stream not found");
+  }
+  return (
+    <div className="p-6">
+      <div className="flex items-center justify-between mb-4">
+        <h1 className="text-2xl font-bold">Keys & Urls</h1>
+        <Button variant="primary">Generate</Button>
+      </div>
+      <div className="space-y-4">
+        <UrlCard value={stream.serverUrl} />
+        <KeyCard value={stream.streamKey} />
+      </div>
+    </div>
+  );
+};
+
+export default KeysPage;
