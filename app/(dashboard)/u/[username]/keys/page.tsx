debb8446a0f300d1b566d6cea66cd770c824dfe1
awais512 <raza.awais25@gmail.com>
2024-03-18 12:11:30 +0000

    Modal

--- app/(dashboard)/u/[username]/keys/page.tsx
+++ app/(dashboard)/u/[username]/keys/page.tsx
@@ -1,9 +1,9 @@
-import { Button } from "@/components/ui/button";
 import React from "react";
 import { UrlCard } from "./_ components/url-card";
 import { getSelf } from "@/lib/auth-service";
 import { getStreamByUserId } from "@/lib/stream-service";
 import { KeyCard } from "./_ components/key-card";
+import { ConnectModal } from "./_ components/connect-modal";
 
 const KeysPage = async () => {
   const self = await getSelf();
@@ -16,7 +16,7 @@ const KeysPage = async () => {
     <div className="p-6">
       <div className="flex items-center justify-between mb-4">
         <h1 className="text-2xl font-bold">Keys & Urls</h1>
-        <Button variant="primary">Generate</Button>
+        <ConnectModal />
       </div>
       <div className="space-y-4">
         <UrlCard value={stream.serverUrl} />
